d71fbad3caead5c1eeb9d698d7cc4b0d60f45193
Chris Jimison <cjimison@gmail.com>
2013-03-08 06:47:27 -0800

    Added support for gtest.

--- gameplay/src/gameplay-main-ios.mm
+++ gameplay/src/gameplay-main-ios.mm
@@ -2,14 +2,21 @@
 
 #import <Foundation/Foundation.h>
 #include "gameplay.h"
-
+#ifdef TESTING
+#include <gtest/gtest.h>
+#endif
 using namespace gameplay;
 
 /**
  * Main entry point.
  */
 int main(int argc, char** argv)
-{   
+{
+#ifdef TESTING
+    testing::InitGoogleTest(&argc, argv);
+    int rez =  RUN_ALL_TESTS();
+    return rez;
+#else
     NSAutoreleasePool *p = [[NSAutoreleasePool alloc] init];
     Game* game = Game::getInstance();
     Platform* platform = Platform::create(game);
@@ -18,6 +25,8 @@ int main(int argc, char** argv)
     delete platform;
     [p release];
     return result;
+#endif
+
 }
 
 #endif
--- gameplay/src/gameplay-main-macosx.mm
+++ gameplay/src/gameplay-main-macosx.mm
@@ -3,6 +3,9 @@
 #import <Foundation/Foundation.h>
 #include "gameplay.h"
 
+#ifdef TESTING
+#include <gtest/gtest.h>
+#endif
 using namespace gameplay;
 
 /**
@@ -10,6 +13,13 @@ using namespace gameplay;
  */
 int main(int argc, char** argv)
 {
+#ifdef TESTING
+    NSLog(@"Running main() from gtest_main.cc\n");
+    testing::InitGoogleTest(&argc, argv);
+    int rez =  RUN_ALL_TESTS();
+    return rez;
+#else
+NSLog(@"Running Normally\n");
     NSAutoreleasePool *p = [[NSAutoreleasePool alloc] init];
     Game* game = Game::getInstance();
     Platform* platform = Platform::create(game);
@@ -18,6 +28,8 @@ int main(int argc, char** argv)
     delete platform;
     [p release];
     return result;
+#endif
+
 }
 
 #endif
